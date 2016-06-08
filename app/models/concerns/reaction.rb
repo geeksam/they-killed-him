55f3046766cd28f3572c61e69c2483ea4634f484
Ronnie Miller <r.miller@crowdcompass.com>
2016-06-07 20:22:39 -0700

    Add random reaction generator

--- app/models/concerns/reaction.rb
+++ app/models/concerns/reaction.rb
@@ -1,10 +1,16 @@
 class Reaction
-  def self.at_random
-    response = RestClient.get( "http://replygif.net/api/tags?api-key=39YAprx5Yi" )
-    tags = JSON.parse(response)
-    title = tags.map {|t| t["title"] }.shuffle.first
+  def self.at_random(title = nil)
+    title ||= tags.sample
     response = RestClient.get( "http://replygif.net/api/gifs", params: { "tag" => title, "api-key" => "39YAprx5Yi" } )
     data = JSON.parse(response)
-    data.first["file"].inspect
+    data.first["file"]
+  end
+
+  def self.tags
+    @tags ||= begin
+      response = RestClient.get( "http://replygif.net/api/tags?api-key=39YAprx5Yi" )
+      tags = JSON.parse(response)
+      tags.map { |t| t['title'] }
+    end
   end
 end
